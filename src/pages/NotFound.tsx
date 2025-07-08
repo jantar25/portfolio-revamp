7596254c3e7f62316d75b9ff6cfb13c445fe515d
Glody Bulambo <bgloire@andrew.cmu.edu>
2025-07-08 23:11:43 +0200

    added not found page

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -2,7 +2,10 @@ import React from 'react'
 
 const NotFound: React.FC = () => {
   return (
-    <div>NotFound</div>
+    <div className='flex flex-col items-center justify-center h-[50vh] xl:h-[70vh]'>
+      <h1 className='text-3xl md:text-5xl xl:text-8xl text-center'>404 - Page Not Found</h1>
+      <p className='text-text-light_extension dark:text-text-dark_extension text-md md:text-xl xl:text-3xl my-8'>Sorry, the page you are looking for does not exist.</p>
+    </div>
   )
 }
 
